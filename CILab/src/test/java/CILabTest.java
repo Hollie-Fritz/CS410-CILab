a7e3ab6eee41d2285a2e23b92e7fc8c410aa06b6
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2022-06-13 16:30:02 -0700

    add tear down

--- CILab/src/test/java/CILabTest.java
+++ CILab/src/test/java/CILabTest.java
@@ -1,7 +1,9 @@
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CILabTest {
@@ -19,16 +21,30 @@ public class CILabTest {
     }
 
     @Test
+    public void getStringTest() {
+        String result = myString.getString();
+        assertEquals(null, result);
+    }
+    @Test
     public void detectCapitalUseTest1() {
         myString.setString("USA");
         assertEquals(true, myString.detectCapitalUse());
 
     }
-
     @Test
     public void detectCapitalUseTest2() {
         myString.setString("leetcode");
         assertEquals(false, myString.detectCapitalUse());
+    }
+
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("lEEtcode");
+        assertEquals(true, myString.detectCapitalUse());
+    }
+
+    @AfterAll
+    static void tearDownAll(){
     }
 
 
